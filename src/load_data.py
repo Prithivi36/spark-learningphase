2369cde1635a4559339acd11024f7309b6c86bf2
Prithivi36 <prithivipappian@gmail.com>
2024-11-12 00:10:55 +0530

    pySpark.sql.types library

--- src/load_data.py
+++ src/load_data.py
@@ -1,6 +1,8 @@
-from idlelib.pyparse import trans
+from shlex import shlex
 
+from pyspark.sql.functions import col
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StringType
 
 spark = SparkSession.builder.appName("loading-app")\
     .master("local[*]").getOrCreate()
@@ -12,3 +14,21 @@ df = spark.read\
 
 df.show()
 df.printSchema()
+
+#targeting cols in ways
+
+col1=df["Close"]
+col2=df.Close
+col3=col("Close")
+
+#access wid variables
+df.select(col1,col2,col3).show()
+
+#access as col as String
+df.select("Close","Open").show()
+
+#casting
+
+data_as_String=df["Date"].cast(StringType()).alias("date")
+
+df.select(df["Date"],data_as_String).show()
